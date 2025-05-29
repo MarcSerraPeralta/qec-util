e00565668e5099137dbdae873a6fa75fa966e6c7
MarcSerraPeralta <marcserraperalta@gmail.com>
2025-05-29 17:06:43 +0200

    Add label options for decompose_into_basis

--- tests/mod2/test_util.py
+++ tests/mod2/test_util.py
@@ -27,3 +27,24 @@ def test_decompose_into_basis():
         _ = decompose_into_basis(vector=b, basis=a)
 
     return
+
+
+def test_decompose_into_basis_from_labels():
+    vector = ["D1", "D2", "D3", "D4"]
+    basis = [["D1", "D2"], ["D2", "D3"], ["D3", "D4"]]
+
+    x = decompose_into_basis(vector=vector, basis=basis)
+
+    assert set(x) == set([0, 2])
+
+    vector = ["D1", "D2", "D3", "D4"]
+    basis = {"Z1": ["D1", "D2"], "Z2": ["D2", "D3"], "Z3": ["D3", "D4"]}
+
+    x = decompose_into_basis(vector=vector, basis=basis)
+
+    assert set(x) == set(["Z1", "Z3"])
+
+    with pytest.raises(TypeError):
+        _ = decompose_into_basis(vector=np.zeros(4, dtype=int), basis=basis)
+
+    return
