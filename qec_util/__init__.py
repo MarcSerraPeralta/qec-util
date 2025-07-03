0c489d52465666b14b5f0651d3a0de57eeecfc0f
MarcSerraPeralta <marcserraperalta@gmail.com>
2025-07-03 19:35:40 +0200

    Update to version 0.4.0

--- qec_util/__init__.py
+++ qec_util/__init__.py
@@ -1,6 +1,6 @@
 from . import performance, data_processing, distance, dems, circuits, dem_instrs
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __all__ = [
     "performance",
     "data_processing",
