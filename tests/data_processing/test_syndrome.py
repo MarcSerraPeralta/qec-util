a450d8d3bb7b112977f6c796bbc7db2d8061e295
MarcSerraPeralta <43704266+MarcSerraPeralta@users.noreply.github.com>
2025-12-09 21:55:40 +0100

    Add data processing for defect probabilities (#103)

--- tests/data_processing/test_syndrome.py
+++ tests/data_processing/test_syndrome.py
@@ -2,7 +2,13 @@ from qec_util.data_processing import syndrome
 
 
 def test_syndrome():
-    methods = ["get_syndromes", "get_defects", "get_final_defects"]
+    methods = [
+        "get_syndromes",
+        "get_defects",
+        "get_final_defects",
+        "get_defect_probs",
+        "get_final_defect_probs",
+    ]
 
     assert set(dir(syndrome)) >= set(methods)
 
