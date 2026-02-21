99268b19ea1dfc342cdbe3f7940c114c35699eea
MarcSerraPeralta <43704266+MarcSerraPeralta@users.noreply.github.com>
2026-02-21 16:27:25 +0100

    Change sampler file format to CSV and add post-selection option (#119)

--- tests/samplers/test_samplers.py
+++ tests/samplers/test_samplers.py
@@ -1,3 +1,4 @@
+import pathlib
 import time
 
 import numpy as np
@@ -12,7 +13,7 @@ from qec_util.samplers import (
 )
 
 
-def test_sampler_to_file(tmp_path):
+def test_sampler_to_file(tmp_path: pathlib.Path):
     circuit = stim.Circuit.generated(
         code_task="repetition_code:memory",
         distance=3,
@@ -22,26 +23,28 @@ def test_sampler_to_file(tmp_path):
     dem = circuit.detector_error_model()
     mwpm = Matching(dem)
 
-    num_failures, num_samples, extra = sample_failures(
+    num_failures, num_samples_ps, num_samples, extra = sample_failures(
         dem,
         mwpm,
         max_samples=1_000,
         max_time=np.inf,
         max_failures=np.inf,
-        file_name=tmp_path / "tmp_file.txt",
+        file_name=tmp_path / "tmp_file.csv",
     )
-    read_failures, read_samples, read_extra = read_failures_from_file(
-        tmp_path / "tmp_file.txt"
+    read_failures, read_samples_ps, read_samples, read_extra = read_failures_from_file(
+        tmp_path / "tmp_file.csv"
     )
 
     assert num_failures == read_failures
+    assert num_samples_ps == read_samples_ps
     assert num_samples == read_samples
-    assert extra == read_extra == []
+    assert extra == read_extra
+    assert "seconds" in extra
 
     return
 
 
-def test_sampler_extra_metrics(tmp_path):
+def test_sampler_extra_metrics(tmp_path: pathlib.Path):
     circuit = stim.Circuit.generated(
         code_task="repetition_code:memory",
         distance=3,
@@ -50,24 +53,27 @@ def test_sampler_extra_metrics(tmp_path):
     )
     dem = circuit.detector_error_model()
     mwpm = Matching(dem)
-    extra_metrics = lambda x: [np.zeros(len(x), dtype=int)]
+    extra_metrics = lambda x: {"test": np.zeros(len(x), dtype=bool)}
 
-    num_failures, num_samples, extra = sample_failures(
+    num_failures, num_samples_ps, num_samples, extra = sample_failures(
         dem,
         mwpm,
-        max_samples=1_000,
-        max_time=np.inf,
-        max_failures=np.inf,
-        file_name=tmp_path / "tmp_file.txt",
+        max_samples=100_000,
+        file_name=tmp_path / "tmp_file_extra_metrics.csv",
+        verbose=False,
         extra_metrics=extra_metrics,
     )
-    read_failures, read_samples, read_extra = read_failures_from_file(
-        tmp_path / "tmp_file.txt"
+    print(num_samples)
+    read_failures, read_samples_ps, read_samples, read_extra = read_failures_from_file(
+        tmp_path / "tmp_file_extra_metrics.csv"
     )
 
     assert num_failures == read_failures
+    assert num_samples_ps == read_samples_ps
     assert num_samples == read_samples
-    assert extra == read_extra == [0]
+    assert extra == read_extra
+    assert extra["test"] == 0
+    assert extra["seconds"] > 0
 
     return
 
@@ -82,30 +88,31 @@ def test_samples_minimum_requirements():
     dem = circuit.detector_error_model()
     mwpm = Matching(dem)
 
-    _, num_samples, _ = sample_failures(
+    _, _, num_samples, _ = sample_failures(
         dem,
         mwpm,
         max_samples=1,
         min_samples=200,
         max_time=0,
         max_failures=1,
+        batch_size=10,
     )
     assert num_samples == 200
 
-    num_failures, _, _ = sample_failures(
+    num_failures, _, _, _ = sample_failures(
         dem,
         mwpm,
-        max_samples=1,
         min_failures=20,
+        max_samples=1,
         max_time=0,
         max_failures=0,
     )
-    assert num_failures >= 2
+    assert num_failures >= 20
 
     return
 
 
-def test_sample_early_stopping(failures_file):
+def test_sample_early_stopping(failures_file: str):
     circuit = stim.Circuit.generated(
         code_task="repetition_code:memory",
         distance=3,
@@ -115,7 +122,7 @@ def test_sample_early_stopping(failures_file):
     dem = circuit.detector_error_model()
     mwpm = Matching(dem)
 
-    num_failures, num_samples, _ = sample_failures(
+    num_failures, num_samples_ps, num_samples, _ = sample_failures(
         dem,
         mwpm,
         max_samples=1,
@@ -125,12 +132,13 @@ def test_sample_early_stopping(failures_file):
     )
 
     assert num_failures == 21
+    assert num_samples_ps == 100
     assert num_samples == 100
 
     return
 
 
-def test_sampler_from_file(failures_file):
+def test_sampler_from_file(failures_file: str):
     circuit = stim.Circuit.generated(
         code_task="repetition_code:memory",
         distance=3,
@@ -140,7 +148,7 @@ def test_sampler_from_file(failures_file):
     dem = circuit.detector_error_model()
     mwpm = Matching(dem)
 
-    num_failures, num_samples, _ = sample_failures(
+    num_failures, num_samples_ps, num_samples, _ = sample_failures(
         dem,
         mwpm,
         max_samples=100,
@@ -150,6 +158,7 @@ def test_sampler_from_file(failures_file):
     )
 
     assert num_failures == 21
+    assert num_samples_ps == 100
     assert num_samples == 100
 
     return
@@ -165,36 +174,53 @@ def test_sampler():
     dem = circuit.detector_error_model()
     mwpm = Matching(dem)
 
-    num_failures, num_samples, _ = sample_failures(
+    num_failures, num_samples_ps, num_samples, _ = sample_failures(
         dem, mwpm, max_samples=1_000, max_time=np.inf, max_failures=np.inf
     )
+    assert num_samples_ps >= 1_000
     assert num_samples >= 1_000
     assert (num_failures >= 0) and (num_samples) >= 0
 
     t_init = time.time()
-    num_failures, num_samples, _ = sample_failures(
-        dem, mwpm, max_samples=np.inf, max_time=1.1, max_failures=np.inf
+    num_failures, num_samples_ps, num_samples, _ = sample_failures(
+        dem, mwpm, max_samples=np.inf, max_time=0.1, max_failures=np.inf
     )
-    assert time.time() - t_init >= 1.1
-    assert (num_failures >= 0) and (num_samples) >= 0
+    assert time.time() - t_init >= 0.1
+    assert (num_failures >= 0) and (num_samples) >= 0 and (num_samples_ps >= 0)
 
-    num_failures, num_samples, _ = sample_failures(
-        dem, mwpm, max_samples=np.inf, max_time=np.inf, max_failures=10
+    num_failures, num_samples_ps, num_samples, _ = sample_failures(
+        dem,
+        mwpm,
+        max_failures=10,
+        batch_size=100_000,
     )
     assert num_failures >= 10
-    assert (num_failures >= 0) and (num_samples) >= 0
+    assert (num_failures >= 0) and (num_samples >= 0) and (num_samples_ps >= 0)
+
+    num_failures, num_samples_ps, num_samples, _ = sample_failures(
+        dem,
+        mwpm,
+        max_samples=2_000,
+        batch_size=1_000,
+        post_selection=lambda x: np.zeros(len(x), dtype=bool),
+    )
+    assert num_samples_ps == 0
+    assert num_samples == 2_000
 
     return
 
 
-def test_read_failures_from_file(failures_file):
-    num_failures, num_samples, _ = read_failures_from_file(failures_file)
+def test_read_failures_from_file(failures_file: str):
+    num_failures, num_samples_ps, num_samples, _ = read_failures_from_file(
+        failures_file
+    )
     assert num_failures == 21
+    assert num_samples_ps == 100
     assert num_samples == 100
     return
 
 
-def test_merge_batches_in_file(tmp_path):
+def test_merge_batches_in_file(tmp_path: pathlib.Path):
     circuit = stim.Circuit.generated(
         code_task="repetition_code:memory",
         distance=3,
@@ -203,83 +229,94 @@ def test_merge_batches_in_file(tmp_path):
     )
     dem = circuit.detector_error_model()
     mwpm = Matching(dem)
-    extra_metrics = lambda x: [np.zeros(len(x), dtype=int)]
+    extra_metrics = lambda x: {"test": np.zeros(len(x), dtype=bool)}
 
-    num_failures, num_samples, extra = sample_failures(
+    num_failures, num_samples_ps, num_samples, extra = sample_failures(
         dem,
         mwpm,
         max_samples=10,
         batch_size=1,
         max_time=np.inf,
         max_failures=np.inf,
-        file_name=tmp_path / "tmp_file.txt",
+        file_name=tmp_path / "tmp_file_merge_batches.csv",
         extra_metrics=extra_metrics,
     )
-    merge_batches_in_file(tmp_path / "tmp_file.txt")
+    merge_batches_in_file(tmp_path / "tmp_file_merge_batches.csv")
 
-    with open(tmp_path / "tmp_file.txt", "r") as file:
+    with open(tmp_path / "tmp_file_merge_batches.csv", "r") as file:
         num_lines = len([l for l in file.read().split("\n") if l != ""])
 
-    assert num_lines == 1
+    assert num_lines == 2  # header + merged line
 
-    read_failures, read_samples, read_extra = read_failures_from_file(
-        tmp_path / "tmp_file.txt"
+    read_failures, read_samples_ps, read_samples, read_extra = read_failures_from_file(
+        tmp_path / "tmp_file_merge_batches.csv"
     )
 
     assert num_failures == read_failures
+    assert num_samples_ps == read_samples_ps
     assert num_samples == read_samples
-    assert extra == read_extra
+    assert extra["test"] == read_extra["test"]
+    assert np.isclose(extra["seconds"], read_extra["seconds"])
 
     return
 
 
-def test_merge_files(tmp_path):
-    with open(tmp_path / "tmp_file_1.txt", "w") as file:
-        file.write("1 10 | 2 4\n")
-    with open(tmp_path / "tmp_file_2.txt", "w") as file:
-        file.write("5 20 | 1 0\n")
+def test_merge_files(tmp_path: pathlib.Path):
+    contents = "num_failures_ps,num_samples_ps,num_samples,seconds,m1,m2\n"
+    contents += "1,10,10,0.003,2,4\n"
+    with open(tmp_path / "tmp_file_1.csv", "w") as file:
+        file.write(contents)
+
+    contents = "num_failures_ps,num_samples_ps,num_samples,seconds,m1,m2\n"
+    contents += "5,20,20,0.004,1,0\n"
+    with open(tmp_path / "tmp_file_2.csv", "w") as file:
+        file.write(contents)
 
     merge_files(
-        [tmp_path / "tmp_file_1.txt", tmp_path / "tmp_file_2.txt"],
-        tmp_path / "merged_file.txt",
+        [tmp_path / "tmp_file_1.csv", tmp_path / "tmp_file_2.csv"],
+        tmp_path / "merged_file.csv",
     )
 
-    num_failures, num_samples, extra = read_failures_from_file(
-        tmp_path / "merged_file.txt"
+    num_failures, num_samples_ps, num_samples, extra = read_failures_from_file(
+        tmp_path / "merged_file.csv"
     )
     assert num_failures == 6
+    assert num_samples_ps == 30
     assert num_samples == 30
-    assert extra[0] == 3
-    assert extra[1] == 4
-    assert len(extra) == 2
+    assert extra["m1"] == 3
+    assert extra["m2"] == 4
+    assert len(extra) == 3
 
-    assert (tmp_path / "tmp_file_1.txt").exists()
-    assert (tmp_path / "tmp_file_2.txt").exists()
+    assert (tmp_path / "tmp_file_1.csv").exists()
+    assert (tmp_path / "tmp_file_2.csv").exists()
 
     merge_files(
-        [tmp_path / "tmp_file_1.txt", tmp_path / "tmp_file_2.txt"],
-        tmp_path / "merged_file.txt",
+        [tmp_path / "tmp_file_1.csv", tmp_path / "tmp_file_2.csv"],
+        tmp_path / "merged_file.csv",
         delete_files=True,
     )
 
-    assert not (tmp_path / "tmp_file_1.txt").exists()
-    assert not (tmp_path / "tmp_file_2.txt").exists()
+    assert not (tmp_path / "tmp_file_1.csv").exists()
+    assert not (tmp_path / "tmp_file_2.csv").exists()
 
-    with open(tmp_path / "merged_file.txt", "w") as file:
-        file.write("5 20 | 1 0\n")
+    contents = "num_failures_ps,num_samples_ps,num_samples,seconds,m1,m2\n"
+    contents += "5,20,20,0.004,1,0\n"
+    with open(tmp_path / "merged_file.csv", "w") as file:
+        file.write(contents)
 
     merge_files(
-        [tmp_path / "merged_file.txt", tmp_path / "merged_file.txt"],
-        tmp_path / "merged_file.txt",
+        [tmp_path / "merged_file.csv", tmp_path / "merged_file.csv"],
+        tmp_path / "merged_file.csv",
     )
 
-    num_failures, num_samples, extra = read_failures_from_file(
-        tmp_path / "merged_file.txt"
+    num_failures, num_samples_ps, num_samples, extra = read_failures_from_file(
+        tmp_path / "merged_file.csv"
     )
     assert num_failures == 5
+    assert num_samples_ps == 20
     assert num_samples == 20
-    assert extra[0] == 1
-    assert extra[1] == 0
-    assert len(extra) == 2
+    assert extra["m1"] == 1
+    assert extra["m2"] == 0
+    assert len(extra) == 3
 
     return
