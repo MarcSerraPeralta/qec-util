9b05e267c8af3af1ab5ad3c8ee37d39e591bbb25
MarcSerraPeralta <marcserraperalta@gmail.com>
2026-03-16 17:28:08 +0100

    Add plot_suppression function

--- tests/performance/test_plots.py
+++ tests/performance/test_plots.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from qec_util.performance.plots import plot_line_threshold
+from qec_util.performance.plots import plot_line_threshold, plot_suppression
 
 
 def test_plot_line_threshold(show_figures):
@@ -14,6 +14,30 @@ def test_plot_line_threshold(show_figures):
     ax = plot_line_threshold(
         ax,
         phys_prob,
+        log_prob,
+        log_prob_lower,
+        log_prob_upper,
+        color="red",
+        label="example",
+    )
+
+    if show_figures:
+        plt.show()
+    plt.close()
+
+    assert isinstance(ax, plt.Axes)
+
+
+def test_plot_suppression(show_figures):
+    distances = np.array([7, 5, 3], dtype=int)
+    log_prob = np.array([1e-4, 1e-3, 1e-2])
+    log_prob_lower = log_prob / 1.2
+    log_prob_upper = log_prob * 1.3
+
+    _, ax = plt.subplots()
+    ax = plot_suppression(
+        ax,
+        distances,
         log_prob,
         log_prob_lower,
         log_prob_upper,
