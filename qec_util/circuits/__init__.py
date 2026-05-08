1f7dd4ca08695b2dba3c3e949b701b760673c863
MarcSerraPeralta <marcserraperalta@gmail.com>
2026-05-08 18:58:29 +0200

    Add remove_observables

--- qec_util/circuits/__init__.py
+++ qec_util/circuits/__init__.py
@@ -4,18 +4,20 @@ from .circuits import (
     move_first_resets_to_beginning,
     move_observables_to_end,
     observables_to_detectors,
-    remove_detectors_except,
+    remove_detectors,
     remove_gauge_detectors,
     remove_non_native_instrs,
+    remove_observables,
 )
 
 __all__ = [
     "remove_gauge_detectors",
-    "remove_detectors_except",
+    "remove_detectors",
     "observables_to_detectors",
     "move_observables_to_end",
     "move_first_resets_to_beginning",
     "format_rec_targets",
     "format_to_rec_targets",
     "remove_non_native_instrs",
+    "remove_observables",
 ]
